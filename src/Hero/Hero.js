fd72f5b386bad77db502efd3230d594bd1caf3c2
sanditya <sanditya2002@gmail.com>
2021-01-05 19:59:20 +0700

    Initial Commit
    Created the Hero Section

---
+++ src/Hero/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="heading">
+        <h1>
+          Improving <span>Reproductive Health</span> For All
+        </h1>
+        <button className="button">
+          <i class="fas fa-search"></i>
+          <p>Our Work</p>
+        </button>
+      </div>
+      <div className="objects"></div>
+    </div>
+  );
+};
+
+export default Hero;
